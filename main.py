63c763d29b25e0160b9558823a6ea1a66d8b27b3
lazymingg <nvminh232@clc.fitus.edu.vn>
2025-03-21 09:25:22 +0700

    adding not more than k trap and not least than k trap

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ grid: List[List[str]] = [['3', ' ', '2', ' '],
                          [' ', ' ', '2', ' '],
                          [' ', '3', '1', ' ']]
 
+def read_input(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        array_2d = [line.strip().split(", ") for line in file]
+    return array_2d
+
+grid = read_input('input_3.txt')
+for row in grid:
+    print(row)
+
+
 def get_neigh(pos: Tuple[int, int], grid: List[List[str]]) -> List[Tuple[int, int]]:
     adjacent = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
     neigh = []
@@ -14,7 +24,7 @@ def get_neigh(pos: Tuple[int, int], grid: List[List[str]]) -> List[Tuple[int, in
         x, y = pos
         x += di
         y += dj
-        if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == ' ':
+        if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == '_':
             neigh.append((x, y))
     return neigh
 
@@ -32,22 +42,29 @@ def convert_int_to_pos(num: int, grid: List[List[str]]) -> Tuple[int, int]:
 
 def generate_trap_combinations(array: List[int], n: int) -> List[List[int]]:
     clauses = []
-    for combo in combinations(array, n):
+    # ít hơn n trap
+    leng = len(array) - n + 1
+    for combo in combinations(array, leng):
         clause = list(combo)
         clauses.append(clause)
+    k = n + 1
+    #không quá n trap
+    if k <= len(array):
+        for combo in combinations(array, k):
+            clause = [-var for var in combo]
+            clauses.append(clause)
     return clauses
 
 def generate_cnf(grid: List[List[str]]) -> List[List[int]]:
     cnf = []
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j] == ' ':
+            if grid[i][j] == '_':
                 continue
             pos = (i, j)
             neigh = get_neigh(pos, grid)
             neigh_vars = [convert_pos_to_int(p, grid) for p in neigh]
-            leng = len(neigh_vars) - int(grid[i][j]) + 1
-            trap_clauses = generate_trap_combinations(neigh_vars, leng)
+            trap_clauses = generate_trap_combinations(neigh_vars, int(grid[i][j]))
             cnf.extend(trap_clauses)
     return cnf
 
@@ -56,7 +73,7 @@ def get_grid_result(grid: List[List[str]], result: List[int]) -> List[List[str]]
     for num in result:
         temp = 'T' if num > 0 else 'G'
         x, y = convert_int_to_pos(num, grid)
-        if new_grid[x][y] == ' ':
+        if new_grid[x][y] == '_':
             new_grid[x][y] = temp
     return new_grid
 
